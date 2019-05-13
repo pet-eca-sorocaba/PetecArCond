7c451974dbdd07a1f3c16460645ddbd939c2729c
Matheus Salaroli <mat.s@terra.com.br>
2019-05-13 19:17:33 -0300

    Novo app

---
+++ Caeli_teste1/app/src/main/java/sorocaba/peteca/com/caeli_teste1/MainActivity.java
@@ -0,0 +1,17 @@
+package sorocaba.peteca.com.caeli_teste1;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
