739aa81ebe4587c9eceb3d0db31e05eda4352877
Matheus Salaroli <mat.s@terra.com.br>
2019-06-13 20:16:04 -0300

    CodigoFinal

--- Codigos/CodigoFinal/CodigoFinal.ino
+++ Codigos/CodigoFinal/CodigoFinal.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DHT.h"
+#include <IRremote.h>
 
+
+#define pinoDHT A1
+
+DHT dht(A1, DHT11);
+IRsend irsend;
+
+int khz = 38;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Inicio do codigo teste");
+    dht.begin();
+
+    unsigned int irSignal[] = {};
+
+    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
+
+    irSignal[] = {};
+
+    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    float temperatura = dht.readTemperature();
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+    Serial.println(" *C");
+    delay(1000);
 
+    if (temperatura < 27) {
+        unsigned int irSignal[] = {};
+        irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
+    }
 }
